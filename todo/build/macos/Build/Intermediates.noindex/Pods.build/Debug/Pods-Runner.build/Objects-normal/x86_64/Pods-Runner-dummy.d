c679c2b06df00dc8f84194af5c16cb7785a1215b
MUJ <99077742+muj-i@users.noreply.github.com>
2023-08-23 03:04:37 +0600

    build removed

--- todo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/HDD-Z/dxProgramming/codeSpace/todo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Volumes/HDD-Z/dxProgramming/codeSpace/todo/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- todo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/x86_64/shared_preferences_foundation_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/HDD-Z/dxProgramming/codeSpace/todo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
-  /Volumes/HDD-Z/dxProgramming/codeSpace/todo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/DerivedSources/shared_preferences_foundation_vers.c
