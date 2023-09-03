5985d819eaaa6c374d747754ec2a3493e6557464
MUJ <99077742+muj-i@users.noreply.github.com>
2023-09-04 04:36:33 +0600

    .

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,143 +1,45 @@
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-        inputDecorationTheme:
-            const InputDecorationTheme(border: OutlineInputBorder())),
-    home: const CrudApiPractice(),
-  ));
+  runApp(MyApp());
 }
 
-class CrudApiPractice extends StatefulWidget {
-  const CrudApiPractice({super.key});
-
-  @override
-  State<CrudApiPractice> createState() => _CrudApiPracticeState();
-}
-
-class _CrudApiPracticeState extends State<CrudApiPractice> {
-  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
-
-  final TextEditingController _name = TextEditingController();
-  final TextEditingController _code = TextEditingController();
-  final TextEditingController _quantity = TextEditingController();
-  final TextEditingController _unitPrice = TextEditingController();
-  final TextEditingController _totalPrice = TextEditingController();
-  final TextEditingController _img = TextEditingController();
-
-  void postRequest() async {
-    Response response = await post(
-      Uri.parse("https://task.teamrabbil.com/api/v1/registration"),
-      headers: {'Content-type': 'application/json'},
-      body: jsonEncode({
-        "email": _name.text.trim(),
-        "firstName": _code.text.trim(),
-        "lastName": _quantity.text.trim(),
-        "mobile": _totalPrice.text.trim(),
-        "password": _unitPrice.text.trim(),
-        "photo": _img.text.trim(),
-      }),
-    );
-    if (response.statusCode == 200) {
-      final decodedBody = jsonDecode(response.body);
-      if (decodedBody['status'] == 'success') {
-        if (mounted) {
-          _code.clear();
-          _name.clear();
-          _quantity.clear();
-          _img.clear();
-          _totalPrice.clear();
-          _unitPrice.clear();
-          ScaffoldMessenger.of(context)
-              .showSnackBar(const SnackBar(content: Text("Success!!")));
-        } else {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(const SnackBar(content: Text("Faild!!")));
-        }
-      }
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: _globalKey,
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _name,
-              decoration: const InputDecoration(hintText: 'email Name'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _code,
-              decoration: const InputDecoration(hintText: 'first Code'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _quantity,
-              decoration: const InputDecoration(hintText: 'last '),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _totalPrice,
-              decoration: const InputDecoration(hintText: 'mobi price'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _unitPrice,
-              decoration: const InputDecoration(hintText: 'pass price'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _img,
-              decoration: const InputDecoration(hintText: 'Image'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return "Fill it";
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  if (_globalKey.currentState!.validate()) {
-                    postRequest();
-                  }
-                },
-                child: const Icon(Icons.arrow_forward_ios))
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Horizontal Grid Row Example'),
+        ),
+        body: HorizontalGridRow(),
+      ),
+    );
+  }
+}
+
+class HorizontalGridRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+      child: Row(
+        children: List.generate(
+          10, // Adjust the number of items as needed
+          (index) {
+            return Container(
+              height: 200, // Adjust the width of your grid items
+              margin: EdgeInsets.all(10.0),
+              child: Card(
+                child: Center(
+                  child: Text(
+                    'Item $index',
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
